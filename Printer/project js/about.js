4e220c6f194965dd4478bf70fa4ba2f6c3f846ea
rimaagg <ghaley.rema@gmail.com>
2025-07-22 14:30:37 +0545

    Add files via upload

---
+++ Printer/project js/about.js
@@ -0,0 +1,6 @@
+var addtocart = document.querySelector("button");
+
+addtocart.addEventListener("click", function(){
+    alert("Added to cart");
+})
+/* pop-up message for when the printer is bought */
